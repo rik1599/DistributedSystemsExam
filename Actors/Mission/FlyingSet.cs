dd4133e479c00129f8f0c7962652f48671dd9502
Emanuele <emanuelelena021099@gmail.com>
2022-09-08 16:41:00 +0200

    strumento per gestire le missioni in volo

---
+++ Actors/Mission/FlyingSet.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actors.Mission
+{
+    /// <summary>
+    /// Strumento per la gestione delle missioni in volo. 
+    /// 
+    /// Quando aggiungo una missione, si registra automaticamente
+    /// la data e l'ora corrente istante della partenza.
+    /// </summary>
+    public class FlyingSet : IMissionSet<FlyingMission>
+    {
+        protected override FlyingMission CreateMission(IActorRef nodeRef, Path path)
+        {
+            // generazione 
+            return new FlyingMission(nodeRef, path, DateTime.Now);
+        }
+    }
+}
