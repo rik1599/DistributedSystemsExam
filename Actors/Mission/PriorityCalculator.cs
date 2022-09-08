60c5704a85124aa406723448b7c1273a79e83d46
Emanuele <emanuelelena021099@gmail.com>
2022-09-08 15:49:14 +0200

    Firma calcolatore priorità

---
+++ Actors/Mission/PriorityCalculator.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actors.Mission
+{
+    /// <summary>
+    /// Strumento per il calcolo della priorità di un nodo 
+    /// </summary>
+    public class PriorityCalculator
+    {
+        /// <summary>
+        /// Calcola la priorità di un certo nodo dato il suo ID, 
+        /// il suo conflict set e il suo flying set.
+        /// </summary>
+        /// <param name="nodeRef">Il mio riferimento</param>
+        /// <param name="conflictSet">I nodi con cui sto negoziando</param>
+        /// <param name="flyingSet">I nodi in volo di cui sto attendendo il termine</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static Priority CalculatePriority(IActorRef nodeRef, Mission[] conflictSet, FlyingMission[] flyingSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
