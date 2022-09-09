40f13a69f66669b3bc3f6b080b8baf01237eedc8
Belliato Riccardo <belliatoriccardo@gmail.com>
2022-09-09 10:36:40 +0200

    starting actor development

--- Actors/DroneActor.cs
+++ Actors/DroneActor.cs
@@ -1,9 +1,45 @@
-﻿using Akka.Actor;
+﻿using Actors.MissionPathPriority;
+using Akka.Actor;
+using Akka.Event;
 
 namespace Actors
 {
     public class DroneActor : ReceiveActor, IWithTimers
     {
         public ITimerScheduler Timers { get; set; }
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+
+        private readonly DateTime _timeSpawn = DateTime.Now;
+        private readonly ISet<IActorRef> _others;
+        private readonly Mission _mission;
+
+        public DroneActor(ISet<IActorRef> others, MissionPath missionPath)
+        {
+            _others = others;
+            _mission = new WaitingMission(Self, missionPath, Priority.NullPriority);
+        }
+
+        #region Behaviours
+
+        private void InitBehaviour()
+        {
+
+        }
+
+        private void NegotiateBehaviour()
+        {
+
+        }
+
+        private void WaitingBehaviour()
+        {
+
+        }
+
+        private void FlyingBehaviour()
+        {
+
+        }
+        #endregion
     }
 }
