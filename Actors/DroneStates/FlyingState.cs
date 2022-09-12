d32294cb3a59b162e0b9a590d08a18f31ed0aa38
Emanuele <emanuelelena021099@gmail.com>
2022-09-12 18:03:43 +0200

    ricezione messaggi esterni dallo stato di volo

--- Actors/DroneStates/FlyingState.cs
+++ Actors/DroneStates/FlyingState.cs
@@ -1,6 +1,9 @@
 ﻿using Actors.Messages.External;
+using Actors.MissionPathPriority;
 using Actors.MissionSets;
 using Akka.Actor;
+using MathNet.Spatial.Euclidean;
+using System.Diagnostics;
 
 namespace Actors.DroneStates
 {
@@ -14,30 +17,75 @@ namespace Actors.DroneStates
 
         internal override DroneActorState RunState()
         {
-            throw new NotImplementedException();
+            foreach (IActorRef node in ConflictSet.GetNodes())
+            {
+                node.Tell(new FlyingResponse(MissionPath));
+            }
+
+            // TODO: avvia attore per la gestione del nodo in volo
+
+            return this;
         }
 
         internal override DroneActorState OnReceive(ConnectRequest msg, IActorRef sender)
         {
-            throw new NotImplementedException();
+            sender.Tell(new FlyingResponse(GetCurrentPath()));
+
+            // se non conosco già il nodo, lo aggiungo alla lista
+            if (!DroneActor.Nodes.Contains(sender))
+                DroneActor.Nodes.Add(sender);
+
+            return this;
         }
 
         internal override DroneActorState OnReceive(ConnectResponse msg, IActorRef sender)
         {
-            throw new NotImplementedException();
+            // TODO: aggiungi errore
+            
+            return this;
         }
 
         internal override DroneActorState OnReceive(FlyingResponse msg, IActorRef sender)
         {
-            throw new NotImplementedException();
+            // TODO: aggiungi errore
+
+            Debug.Assert(GetCurrentPath().ClosestConflictPoint(msg.Path) == null);
+
+            return this;
         }
 
         internal override DroneActorState OnReceive(MetricMessage msg, IActorRef sender)
         {
-            throw new NotImplementedException();
+            // TODO: aggiungi errore
+
+            sender.Tell(new MetricMessage(Priority.InfinitePriority));
+            sender.Tell(new FlyingResponse(GetCurrentPath()));
+
+            return this;
         }
 
         internal override DroneActorState OnReceive(WaitMeMessage msg, IActorRef sender)
+        {
+            // TODO: aggiungi errore
+
+            sender.Tell(new FlyingResponse(GetCurrentPath()));
+
+            return this;
+        }
+
+        private MissionPath GetCurrentPath()
+        {
+            // TODO: effettua ASK ad attore volo
+            Point2D currentPosition = GetCurrentPosition();
+
+            return new MissionPath(
+                    currentPosition,
+                    MissionPath.EndPoint,
+                    MissionPath.Speed
+                    );
+        }
+
+        private Point2D GetCurrentPosition()
         {
             throw new NotImplementedException();
         }
