5152f6f439e69bab642e0de309f35ac9b1f7a511
Emanuele <emanuelelena021099@gmail.com>
2022-09-16 19:34:33 +0200

    Messaggi per richiesta stato corrente.

---
+++ Actors/Messages/UserMessages.cs
@@ -0,0 +1,30 @@
+﻿using Actors.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actors.Messages.User
+{
+    /// <summary>
+    /// Messaggio di richiesta dello stato corrente
+    /// </summary>
+    public class GetStatusRequest
+    {
+
+    }
+
+    /// <summary>
+    /// Stato corrente (ritornato come DTO)
+    /// </summary>
+    public class GetStatusResponse
+    {
+        public DroneStateDTO StateDTO { get; }
+
+        public GetStatusResponse(DroneStateDTO stateDTO) 
+        {  
+            StateDTO = stateDTO;
+        }
+    }
+}
