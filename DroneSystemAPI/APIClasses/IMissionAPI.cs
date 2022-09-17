faada0c4a4d3b9847654892debbc58199b5c1192
Emanuele <emanuelelena021099@gmail.com>
2022-09-17 11:36:09 +0200

    interfaccia e implementazione di base dell'API OO al sistema

---
+++ DroneSystemAPI/APIClasses/IMissionAPI.cs
@@ -0,0 +1,56 @@
+﻿using Actors.DTO;
+using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystemAPI.APIClasses
+{
+    /// <summary>
+    /// API per interfacciarsi con una missione remota.
+    /// </summary>
+    public interface IMissionAPI
+    {
+        /// <summary>
+        /// Riferimento al drone che sta portando a termine la missione remota
+        /// </summary>
+        /// <returns></returns>
+        public IActorRef GetDroneRef();
+
+        /// <summary>
+        /// Richiedi lo stato corrente del drone e della missione
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MissionIsUnreachableException">Se l'attore non è raggiungibilie</exception>
+        public Task<DroneStateDTO> GetCurrentStatus();
+
+        /// <summary>
+        /// Richiedi la cancellazione della missione.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MissionIsUnreachableException">Se l'attore non è raggiungibilie</exception>
+        public Task Cancel(); 
+    }
+
+    /// <summary>
+    /// Strumento per creare istanze di <code>IMissionAPI</code> 
+    /// A PARTIRE DA UNA MISSIONE GIA' AVVIATA.
+    /// </summary>
+    public interface IMissionAPIFactory
+    {
+        /// <summary>
+        /// Crea un'istanza API per interfacciarti ad 
+        /// una missione già esistente.
+        /// </summary>
+        /// <param name="nodeRef"></param>
+        /// <returns></returns>
+        public IMissionAPI GetMissionAPI(IActorRef nodeRef);
+    }
+
+    public class MissionIsUnreachableException : AkkaException
+    {
+
+    }
+}
---
+++ DroneSystemAPI/APIClasses/SimpleMissionAPI/SimpleMissionAPI.cs
@@ -0,0 +1,63 @@
+﻿using Actors.DTO;
+using Actors.Messages.User;
+using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystemAPI.APIClasses.SimpleMissionAPI
+{
+    internal class SimpleMissionAPI : IMissionAPI
+    {
+        public static readonly TimeSpan DEFAULT_TIMEOUT = new TimeSpan(0, 0, 10);
+
+
+        private readonly TimeSpan _timeout = DEFAULT_TIMEOUT;
+        private readonly IActorRef _nodeRef;
+
+        public SimpleMissionAPI(IActorRef nodeRef)
+        {
+            _nodeRef = nodeRef;
+        }
+
+        public SimpleMissionAPI(IActorRef nodeRef, TimeSpan timeout) : this(nodeRef)
+        {
+            _timeout = timeout;
+        }
+
+        public IActorRef GetDroneRef() => _nodeRef;
+
+        public async Task<DroneStateDTO> GetCurrentStatus()
+        {
+            Task<GetStatusResponse> t = GetDroneRef()
+                .Ask<GetStatusResponse>(new GetStatusRequest(), _timeout);
+
+
+            return new Task<DroneStateDTO>(() =>
+            {
+                t.Wait();
+                if (t.IsFaulted || !t.IsCompleted)
+                {
+                    throw new MissionIsUnreachableException();
+                }
+
+                return t.Result.StateDTO;
+            }).Result;        
+        }
+
+        public Task Cancel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class SimpleMissionAPIFactory : IMissionAPIFactory
+    {
+        public IMissionAPI GetMissionAPI(IActorRef nodeRef)
+        {
+            return new SimpleMissionAPI(nodeRef);
+        }
+    }
+}
