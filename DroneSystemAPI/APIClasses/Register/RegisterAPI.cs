2e85b22ea0f4cd30a58bcd9ae1d971b937823acc
Emanuele <emanuelelena021099@gmail.com>
2022-09-17 14:34:44 +0200

    register API + rinomina actor spawner in provider

---
+++ DroneSystemAPI/APIClasses/Register/RegisterAPI.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystemAPI.APIClasses.Register
+{
+    public class RegisterAPI
+    {
+        public IActorRef RegisterRef { get; }
+
+        public RegisterAPI(IActorRef registerRef)
+        {
+            RegisterRef = registerRef;
+        }
+    }
+}
--- DroneSystemAPI/APIClasses/Utils/ActorProvider.cs
+++ DroneSystemAPI/APIClasses/Utils/ActorProvider.cs
@@ -9,17 +9,17 @@ using System.Threading.Tasks;
 namespace DroneSystemAPI.APIClasses.Utils
 {
     /// <summary>
-    /// Strumento per:
-    /// 
+    /// Strumento unico per:
     /// - reperire facilmente gli indirizzi di attori collocati su sistemi noti
     /// - spawnare attori (localmente o su sistemi remoti avviati)
+    /// 
     /// </summary>
-    public class ActorSpawner
+    public class ActorProvider
     {
         public static readonly TimeSpan DEFAULT_TIMEOUT = new TimeSpan(0, 0, 10);
         private readonly TimeSpan _timeout = DEFAULT_TIMEOUT;
 
-        public ActorSpawner(TimeSpan timeout) 
+        public ActorProvider(TimeSpan timeout) 
         {
             _timeout = timeout;
         }
