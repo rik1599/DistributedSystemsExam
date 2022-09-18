f8ba2a16f467fe3abfdc3f0a759961a28de3f0ca
Emanuele <emanuelelena021099@gmail.com>
2022-09-18 16:58:25 +0200

    Attore osservatore che accumula notifiche in una coda

---
+++ DroneSystemAPI/APIClasses/Mission/ObserverMissionAPI/ObserverMessages.cs
@@ -0,0 +1,26 @@
+﻿using Actors.Messages.StateChangeNotifier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSystemAPI.APIClasses.Mission.ObserverMissionAPI
+{
+    internal class AskForNotifications
+    {
+    }
+
+    internal class Notifications
+    {
+        /// <summary>
+        /// Lista ordinata delle nuove notifiche
+        /// </summary>
+        public IReadOnlyList<OrderedStateChangeNotification> NewNotifications;
+
+        public Notifications(IReadOnlyList<OrderedStateChangeNotification> newNotifications)
+        {
+            NewNotifications = newNotifications;
+        }
+    }
+}
